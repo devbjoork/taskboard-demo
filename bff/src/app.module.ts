f96aca42f03fe05e8068c707951b25942d53db4b
devbjoork <bjoorko@gmail.com>
2023-03-17 03:14:26 +0300

    Reconfigured bff db connection

--- bff/src/app.module.ts
+++ bff/src/app.module.ts
@@ -6,9 +6,22 @@ import { BoardModule } from './board/board.module';
 import { ColumnModule } from './column/column.module';
 import { TaskModule } from './task/task.module';
 
+const IS_DEV = process.env.NODE_ENV === 'dev';
+const PORT = 27017;
+const DB = 'taskboard';
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/taskboard'),
+    MongooseModule.forRoot(
+      `mongodb://${IS_DEV ? '127.0.0.1' : process.env.DB_URI}:${PORT}`,
+      {
+        auth: {
+          username: `${IS_DEV ? '' : process.env.DB_USERNAME}`,
+          password: `${IS_DEV ? '' : process.env.DB_PASSWORD}`,
+        },
+        dbName: `${DB}`,
+      },
+    ),
     BoardModule,
     ColumnModule,
     TaskModule,
